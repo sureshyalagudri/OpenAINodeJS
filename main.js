1b9cfe01cec69d47a1fc665a89ba5dc1ef314ebc
suresh yalagudri <suresh.yalagudri@gmail.com>
2025-03-11 04:05:45 +0000

    first api call with open ai

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 import { generateToken } from './util.js';
 import { OpenAI  } from 'openai';
-import dotenv from "dotenv";
 
 await generateToken()
-dotenv.config();
-console.log(process.env.OPENAI_API_KEY)
+
+const header_name = process.env.GATEWAY_HEADER_NAME
+const header_value = process.env.GATEWAY_HEADER_VALUE
+const headers = {
+    [header_name]: header_value,
+};
+const client = new OpenAI({defaultHeaders: headers});
+
+const completion = await client.chat.completions.create({
+    model: "gpt-4o-2024-05-13",
+    messages: [
+        { role: "user", content: "What is OpenAI" }
+    ]
+});
+console.log(completion.choices[0].message);
